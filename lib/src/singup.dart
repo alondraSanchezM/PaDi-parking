54d61793bcf3c1eed6bcd8e49aa1d098628eb535
alo_sanchez <alondra1d35@gmail.com>
2021-04-19 01:54:14 -0500

    UI - singup

--- lib/src/singup.dart
+++ lib/src/singup.dart
@@ -11,22 +11,26 @@ class SignUpPage extends StatefulWidget {
 }
 
 class _SignUpPageState extends State<SignUpPage> {
-  Widget _entryField(String title, {bool isPassword = false}) {
+  Widget _entryField(String title, String hint, {bool isPassword = false}) {
     return Container(
-      margin: EdgeInsets.symmetric(vertical: 10),
+      margin: EdgeInsets.symmetric(vertical: 4),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: <Widget>[
           Text(
             title,
-            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 15),
+            style: TextStyle(
+                fontWeight: FontWeight.w700,
+                fontSize: 10,
+                color: Color(0x99000000)),
           ),
           SizedBox(
-            height: 10,
+            height: 6,
           ),
           TextField(
               obscureText: isPassword,
               decoration: InputDecoration(
+                  hintText: hint,
                   border: InputBorder.none,
                   fillColor: Color(0xfff3f3f4),
                   filled: true))
@@ -34,7 +38,6 @@ class _SignUpPageState extends State<SignUpPage> {
       ),
     );
   }
-
 
   Widget _loginAccountLabel() {
     return InkWell(
@@ -50,16 +53,16 @@ class _SignUpPageState extends State<SignUpPage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Text(
-              'Already have an account ?',
+              '¿Ya tienes una cuenta?',
               style: TextStyle(fontSize: 13, fontWeight: FontWeight.w600),
             ),
             SizedBox(
               width: 10,
             ),
             Text(
-              'Login',
+              'Inicia sesión',
               style: TextStyle(
-                  color: Color(0xfff79c4f),
+                  color: Color(0xffCFD11A),
                   fontSize: 13,
                   fontWeight: FontWeight.w600),
             ),
@@ -72,10 +75,95 @@ class _SignUpPageState extends State<SignUpPage> {
   Widget _emailPasswordWidget() {
     return Column(
       children: <Widget>[
-        _entryField("Username"),
-        _entryField("Email id"),
-        _entryField("Password", isPassword: true),
+        _entryField("CORREO ELECTRÓNICO", "tunombre@email.com"),
+        SizedBox(height: 10),
+        _entryField("NOMBRE", "Juan"),
+        SizedBox(height: 10),
+        _entryField("APELLIDO", "Miranda"),
+        SizedBox(height: 10),
+        _entryField("CONTRASEÑA", "*******", isPassword: true),
       ],
+    );
+  }
+
+  Widget _labelOr() {
+    return Container(
+        margin: EdgeInsets.only(top: 15, bottom: 10),
+        child: Column(
+          children: <Widget>[
+            Text(
+              'o con tu correo electrónico',
+              style: TextStyle(color: Color(0xd9808F85), fontSize: 12),
+            ),
+          ],
+        ));
+  }
+
+  Widget _facebookButton() {
+    return Container(
+      height: 50,
+      margin: EdgeInsets.symmetric(vertical: 10),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.all(Radius.circular(10)),
+      ),
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            flex: 1,
+            child: Container(
+              decoration: BoxDecoration(
+                color: Color(0xff1959a9),
+                borderRadius: BorderRadius.only(
+                    bottomLeft: Radius.circular(5),
+                    topLeft: Radius.circular(5)),
+              ),
+              alignment: Alignment.center,
+              child: Text('f',
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 25,
+                      fontWeight: FontWeight.w400)),
+            ),
+          ),
+          Expanded(
+            flex: 5,
+            child: Container(
+              decoration: BoxDecoration(
+                color: Color(0xff2872ba),
+                borderRadius: BorderRadius.only(
+                    bottomRight: Radius.circular(5),
+                    topRight: Radius.circular(5)),
+              ),
+              alignment: Alignment.center,
+              child: Text('CREA UNA CUENTA CON FACEBOOK',
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 14,
+                      fontWeight: FontWeight.w700)),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _submitButton() {
+    return Container(
+      width: 149,
+      padding: EdgeInsets.symmetric(vertical: 15),
+      alignment: Alignment.center,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.all(Radius.circular(5)),
+        color: Color(0xff91C499),
+      ),
+      child: Text(
+        'CREAR CUENTA',
+        style: TextStyle(
+          fontSize: 14,
+          color: Colors.white,
+          fontWeight: FontWeight.w700,
+        ),
+      ),
     );
   }
 
@@ -83,15 +171,28 @@ class _SignUpPageState extends State<SignUpPage> {
   Widget build(BuildContext context) {
     final height = MediaQuery.of(context).size.height;
     return Scaffold(
+      appBar: AppBar(
+        title: Text('Registro'),
+        backgroundColor: Color(0xff0C2431),
+      ),
+      drawer: Drawer(
+          child: new ListView(
+        children: <Widget>[
+          Text('Primero elemento!'),
+          //Menu deisy
+        ],
+      )),
       body: Container(
         height: height,
         child: Stack(
           children: <Widget>[
-           // Positioned(
-           //   top: -MediaQuery.of(context).size.height * .15,
-           //   right: -MediaQuery.of(context).size.width * .4,
-              //child: BezierContainer(),
-           // ),
+            Positioned.fill(
+              child: Image.asset(
+                "assets/low-shape.png",
+                fit: BoxFit.fitWidth,
+                alignment: Alignment.bottomLeft,
+              ),
+            ),
             Container(
               padding: EdgeInsets.symmetric(horizontal: 20),
               child: SingleChildScrollView(
@@ -99,16 +200,15 @@ class _SignUpPageState extends State<SignUpPage> {
                   crossAxisAlignment: CrossAxisAlignment.center,
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: <Widget>[
-                    SizedBox(height: height * .2),
-                    SizedBox(
-                      height: 50,
-                    ),
+                    SizedBox(height: 24),
+                    _facebookButton(),
+                    _labelOr(),
+                    SizedBox(height: 24),
                     _emailPasswordWidget(),
-                    SizedBox(
-                      height: 20,
-                    ),
-                    SizedBox(height: height * .14),
                     _loginAccountLabel(),
+                    SizedBox(height: height * .10),
+                      _submitButton(),
+                    SizedBox(height: 24),
                   ],
                 ),
               ),
